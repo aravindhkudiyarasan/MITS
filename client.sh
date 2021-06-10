70876388c08579f4e674bbf8174e51c925ebbd06
Aravindh K Rocker <aravindh.kudiyarasu@gmail.com>
2021-06-10 23:16:41 +0530

    Delete client.sh

--- client.sh
+++
@@ -1,6 +0,0 @@
-for servers in $(cat hosts); do
-#  touch $servers.cfg
-  cat client.cfg > /usr/local/nagios/etc/servers/$servers.cfg
-  sed -i "s/test1/$servers/g" /usr/local/nagios/etc/servers/$servers.cfg
-  cat /usr/local/nagios/etc/servers/$servers.cfg
-done
